e32876412158465f4e429554c683f9ea214e4165
Dmytro Posokhov <dmytro.posokhov@namecheap.com>
2022-09-16 19:16:05 +0300

    second commit

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,34 @@
+import pandas as pd
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+
+#  create model of goods with fields: name, price, description with regex validator, date_added
+
+class Goods(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    # create validator for field 'date_added' witch should be more than current date
+    def clean(self):
+        if self.date_added < pd.Timestamp.now():
+            raise ValidationError('Date of creation should be more than current date')
+
+
+# create regular expression 8 symbols and 2 digits, upper and lower case letters and special characters
+REGEX_NAME = r'^[A-Za-z0-9]{8}[A-Za-z0-9]{2}$'
+
+
+
+
+
+
+
+
+
+
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from shop.models import Goods
+import pandas as pd
 
-# Create your views here.
+
+# create list of goods ordered by date of creation and pagination 20 items by page
+class GoodsListView(ListView):
+    model = Goods
+    ordering = ('-date_added',)
+    template_name = 'shop/goods_list.html'
+    paginate_by = 20
+
+    # create validator for field 'date_added' witch should be more than current date
+    def get_queryset(self):
+        return Goods.objects.filter(date_added__gt=pd.Timestamp.now())
+
+
+
+
+
+
+
+
+
+
+#
+# # example
+# record = {
+#     'Date': ['2022-01-01', '2022-02-02', '2022-03-04', '2022-04-05', '2022-05-08', '2022-06-09'],
+#     'B': [22, None, None, None, 18, 30]
+# }
+# df = pd.DataFrame(record, columns=['Date', 'B'])
+#
+# # replace None with previous value
+# df['B'].fillna(method='ffill', inplace=True)
+
+
